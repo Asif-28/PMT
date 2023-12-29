10f9282ed26ffdaa59e551b9fdf832aeda0ffeeb
Rishang <rishangbhavsarcs@gmail.com>
2023-12-29 21:15:08 +0530

    target

--- backend/models.py
+++ backend/models.py
@@ -10,8 +10,8 @@ class Project(BaseModel):
     IncidenceRate: str
     Loi: str
     Scope: int
-    SelectedOption: str
     Target: str
+    TargetDescription: str
     SelectedCountry: str
     OnlineOffline: str
     SelectedDiv: str
@@ -27,6 +27,14 @@ class Project(BaseModel):
     def append_min(cls, v):
         if v is not None:
             return f"{v} Min"
+        return v
+    
+    @validator("Target", pre=True, always=True)
+    def validate_target(cls, v):
+        # check if Target is one of those values
+        if v is not None:
+            if v not in ["HCP", "B2B", "B2C"]:
+                raise ValueError("Target must be one of GenPop, B2B, B2C")
         return v
 
     @staticmethod
