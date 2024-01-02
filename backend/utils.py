0b6d001fddec9636380ba8e3c765e2aaf66b5f3d
Rishang <rishangbhavsarcs@gmail.com>
2024-01-02 19:04:36 +0530

    add: util

---
+++ backend/utils.py
@@ -0,0 +1,8 @@
+from bson.objectid import ObjectId
+
+def validate_object(cls, v):
+    # check if user_id is valid
+    if v is not None:
+        if not ObjectId.is_valid(v):
+            raise ValueError("user_id is not valid")
+    return v
