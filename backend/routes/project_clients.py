c9cecc4653c2e952ad629902f3ffaef1d4f17791
Rishang <rishangbhavsarcs@gmail.com>
2024-01-08 17:59:13 +0530

    fix: imports

--- backend/routes/project_clients.py
+++ backend/routes/project_clients.py
@@ -1,8 +1,6 @@
-import mongoengine
 from fastapi import APIRouter
 from ..utils import message, JSONResponse
 from ..models.project_client import ProjectClient, ProjectClientModel
-import pymongo
 
 
 router = APIRouter()
