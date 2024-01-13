27a84163532c506c56c160732300ff55b851d5cc
Rishang <rishangbhavsarcs@gmail.com>
2024-01-13 15:08:11 +0530

    a

--- backend/ptm/app/admin.py
+++ backend/ptm/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import (
+    Client,
+    ProjectCreation,
+    ProjectClient,
+)
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(ProjectCreation)
+admin.site.register(ProjectClient)
