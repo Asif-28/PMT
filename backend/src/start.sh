492e10653df77410e4843bc85d5068131f15de41
Rishang <rishangbhavsarcs@gmail.com>
2024-01-13 15:41:37 +0530

    a

--- backend/src/start.sh
+++ backend/src/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+
+python3 manage.py runserver 0.0.0.0:8000
