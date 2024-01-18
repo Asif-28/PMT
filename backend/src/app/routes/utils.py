5e3655b9835b3f208e1ba159067bf03a862deeac
Rishang <rishangbhavsarcs@gmail.com>
2024-01-18 20:45:21 +0530

    fix

--- backend/src/app/routes/utils.py
+++ backend/src/app/routes/utils.py
@@ -1,26 +1,21 @@
 from ninja import Router
 from .._country_codes import countries
-from django.http import HttpResponse
+from ..utils import get_request_ip
+from ninja.schema import Schema
 
 router = Router()
 
+""" Schemas """
+class IPResponse(Schema):
+    ip: str
+
+
+""" Routes """
 
 @router.get("/countries")
 def list_countries(request):
     return countries
 
-
 @router.get("/ip")
-def get_user_ip(request):
-    # Try to get the real IP by checking the 'HTTP_X_FORWARDED_FOR' header
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(",")[
-            0
-        ]  # In case there is a 'forwarded for' chain, take the first one
-    else:
-        ip = request.META.get(
-            "REMOTE_ADDR"
-        )  # Otherwise, use the standard 'REMOTE_ADDR'
-
-    return HttpResponse(f"Your IP address is {ip}")
+def get_ip(request):
+    return IPResponse(ip=get_request_ip(request))
