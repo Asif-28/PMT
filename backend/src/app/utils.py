cdce9525e258222c9debb320ecd61b8768259252
Rishang <rishangbhavsarcs@gmail.com>
2024-01-29 16:02:13 +0530

    a

--- backend/src/app/utils.py
+++ backend/src/app/utils.py
@@ -55,6 +55,38 @@ def get_request_ip(request):
 
 
 def validate_ipqualityscore(ip, api_key):
+    """
+    Validate IP address using IPQualityScore API
+    example response:
+        {
+        "success": true,
+        "message": "Success",
+        "fraud_score": 100,
+        "country_code": "AT",
+        "region": "Vienna",
+        "city": "Vienna",
+        "ISP": "Aeza International",
+        "ASN": 210644,
+        "organization": "Aeza International",
+        "is_crawler": false,
+        "timezone": "Europe\/Vienna",
+        "mobile": false,
+        "host": "tor-exit.node",
+        "proxy": true,
+        "vpn": true,
+        "tor": true,
+        "active_vpn": false,
+        "active_tor": true,
+        "recent_abuse": true,
+        "bot_status": true,
+        "connection_type": "Premium required.",
+        "abuse_velocity": "Premium required.",
+        "zip_code": "N\/A",
+        "latitude": 48.2,
+        "longitude": 16.37,
+        "request_id": "KbbpJg7pk9"
+    }
+    """
     return request_session.get(
         f"https://ipqualityscore.com/api/json/ip/{api_key}/{ip}?strictness=2&fast=1"
     ).json()
