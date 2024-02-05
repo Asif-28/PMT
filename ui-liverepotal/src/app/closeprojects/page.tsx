d1295ccd1b920e1e837842adf5d390cde95faacb
Asif Ali <asif.a.7277@gmail.com>
2024-02-06 00:11:28 +0530

    added the legacy live-repotal

---
+++ ui-liverepotal/src/app/closeprojects/page.tsx
@@ -0,0 +1,87 @@
+import ClosedProjectComponent from "@/components/closeproject/CloseProject";
+import React from "react";
+
+const projects = [
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+  {
+    name: "B2B Decision Makers Survey 2023",
+    ir: 38,
+    loi: 22,
+    scope: 20,
+    achieved: 200,
+    remaining: 109,
+    status: "Closed",
+  },
+];
+
+const ClosedProjects = () => {
+  return (
+    <div>
+      <ClosedProjectComponent projects={projects} />
+    </div>
+  );
+};
+
+export default ClosedProjects;
---
+++ ui-liverepotal/src/app/page.tsx
@@ -0,0 +1,11 @@
+import MyChart from "@/components/linechart/LineChart";
+import ChartViewer from "@/components/linechart/LineChart";
+
+export default function Home() {
+  return (
+    <main>
+      {" "}
+      <MyChart />
+    </main>
+  );
+}
