37adca4ade5fdbc89106ac31e5b18396951f8dd7
Asif Ali <asif.a.7277@gmail.com>
2024-02-07 17:01:14 +0530

    remove errors

--- ui-liverepotal/src/components/projectdetails/TimeZones.tsx
+++ ui-liverepotal/src/components/projectdetails/TimeZones.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import ct from "countries-and-timezones";
 
 const TimeZones = () => {
-  return <div>TimeZones</div>;
+  const country = ct.getCountry("DE");
+  console.log(country);
+  return <div>{/* <h1> {country}</h1> */}</div>;
 };
 
 export default TimeZones;
