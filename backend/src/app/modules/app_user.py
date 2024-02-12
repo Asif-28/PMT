0423996428d5cdd5be300861161dee313e50ffa9
Rishang <rishangbhavsarcs@gmail.com>
2024-02-12 23:14:14 +0530

    add

--- backend/src/app/modules/app_user.py
+++ backend/src/app/modules/app_user.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class AppUser(models.Model):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(db_default=True)
     token = models.CharField(max_length=255, blank=True, null=True, db_index=True)
     role = models.CharField(max_length=255, db_default="user")
     last_token_refresh = models.DateTimeField(auto_now=True)
