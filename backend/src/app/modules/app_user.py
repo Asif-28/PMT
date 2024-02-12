290522423dc062361fb4653edf123eb1042c6dfa
Rishang <rishangbhavsarcs@gmail.com>
2024-02-12 18:55:02 +0530

    add

--- backend/src/app/modules/app_user.py
+++ backend/src/app/modules/app_user.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class User(User):
+    ...
     # You can add additional fields here if required
-    username = models.CharField()
-    email = models.EmailField()
-    password = models.CharField()
+    # username = models.CharField()
+    # email = models.EmailField()
+    # password = models.CharField()
     # groups = models.ManyToManyField()
