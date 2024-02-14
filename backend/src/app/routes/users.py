eac48d7f1e621977df57b746e4734810f6e75512
Random._ <rishangbhavsarcs@gmail.com>
2024-02-14 19:00:30 +0530

    Fix models (#82)
    * fix: auth

--- backend/src/app/routes/users.py
+++ backend/src/app/routes/users.py
@@ -1,7 +1,14 @@
 from ninja import Router
 from ninja.security import HttpBearer
 from ninja import Form
+import json
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import (
+    csrf_exempt,
+    ensure_csrf_cookie,
+    requires_csrf_token,
+)
 from ..modules.app_user import AppUser
 from ..modules._custom_schemas import AppUserSchema
 from ..utils import uniq_md5_hash
@@ -9,8 +16,46 @@ from ..utils import uniq_md5_hash
 router = Router()
 
 
+@router.post("/csrf", auth=None)
+@ensure_csrf_cookie
+@csrf_exempt
+def get_csrf_token(request):
+    return HttpResponse()
+
+
+@router.get("/xcsrf", auth=None)
+@requires_csrf_token
+def read_user(request):
+    data: HttpResponse = render(request, "csrf.html", {})
+    # get csrf token from data
+    csrf_token = (
+        data.content.decode()
+        .split('name="csrfmiddlewaretoken" value="')[1]
+        .split('"')[0]
+    )
+    _d = {
+        "csrf_token": request.COOKIES.get("csrftoken"),
+        "x_csrf_token": csrf_token,
+    }
+
+    return HttpResponse(
+        content=json.dumps(_d),
+        content_type="application/json",
+        headers={"X-CSRFToken": csrf_token},
+    )
+
+
 @router.post("/create")
 def create_user(request, user_in: AppUserSchema):
+    # get token from request
+    # token = request.cookies.get("X-API-KEY")
+    # if not token:
+    #     return {"error": "Not authenticated"}
+    # user = AppUser.objects.filter(token=token).first()
+
+    # if user.role != "admin":
+    #     return {"error": f"Not {user.role} authorized to create user"}
+
     user = AppUser.objects.create(
         username=user_in.username,
         email=user_in.email,
