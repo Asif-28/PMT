52708141066ece6cddc20699a7380ba9e21bbacd
Rishang <rishangbhavsarcs@gmail.com>
2024-02-16 00:42:54 +0530

    fix

--- backend/src/app/routes/auth.py
+++ backend/src/app/routes/auth.py
@@ -1,4 +1,6 @@
+from django.utils import timezone
 from ninja.security import HttpBearer
+from ..utils import message
 from ..modules.app_user import AppUser
 
 
@@ -7,7 +9,16 @@ class HeaderAuth(HttpBearer):
         super().__init__()
 
     def authenticate(self, request, token):
+
+        # seconds
+        max_age = 36000.0
+        
         if token:
             user = AppUser.objects.get(token=token)
             if user:
-                return token
+                # return token
+                if (timezone.now() - user.last_token_refresh).total_seconds() < max_age:
+                    return token
+                else:
+                    raise message.error("Token expired")
+
